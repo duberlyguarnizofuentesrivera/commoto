1b54fb0b4b358785f4510a98e10de6e0f81faa20
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2022-07-21 13:39:57 -0500

    First commit. Initial empty state.

---
+++ src/main/java/com/mdsperu/commoto/CommotoApplication.java
@@ -0,0 +1,13 @@
+package com.mdsperu.commoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdsperu/commoto/CommotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsperu.commoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
